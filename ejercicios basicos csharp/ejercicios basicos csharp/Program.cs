b55e5def3382c7dd3a6b6aeaa24cc1820b3982c0
AndresYzimar CanoRios <acano4@ucol.mx>
2020-10-16 13:29:33 -0500

    n1

---
+++ ejercicios basicos csharp/ejercicios basicos csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejercicios_basicos_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
