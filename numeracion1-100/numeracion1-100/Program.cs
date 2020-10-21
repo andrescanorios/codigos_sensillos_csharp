d4ce09211950bd8cbcf8d3921ec6b4d9cbd2f5c1
AndresYzimar CanoRios <acano4@ucol.mx>
2020-10-21 09:59:06 -0500

    segundo codigo

--- numeracion1-100/numeracion1-100/Program.cs
+++ numeracion1-100/numeracion1-100/Program.cs
@@ -6,11 +6,26 @@ namespace numeracion1_100
     {
         static void Main(string[] args)
         {
+            /*
+             * primer codigo con un siclo for en csharp
             for (int i=1; i <= 100; i++)
             {
                 Console.Write(i);
                 Console.Write("-");
             }
+            */
+            int i, suma = 0, valor, promedio;
+            string linea;
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("Ingresa el valor: ");
+                linea = Console.ReadLine();
+                valor = int.Parse(linea);
+                suma = suma + valor;
+            }
+            Console.WriteLine("La suma es: {0}", suma);
+            promedio = suma / 10;
+            Console.Write("La promedio es: {0}", promedio);
         }
     }
 }
