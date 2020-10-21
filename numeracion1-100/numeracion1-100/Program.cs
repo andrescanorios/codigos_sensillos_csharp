edef39a41e956e247d3d36431e7b61cee2e7ad36
AndresYzimar CanoRios <acano4@ucol.mx>
2020-10-21 10:09:19 -0500

    ultimo codigo del dia 21/10/2020

--- numeracion1-100/numeracion1-100/Program.cs
+++ numeracion1-100/numeracion1-100/Program.cs
@@ -14,6 +14,8 @@ namespace numeracion1_100
                 Console.Write("-");
             }
             */
+            /*
+             * este es el segundo codigo la suma y el promedio
             int i, suma = 0, valor, promedio;
             string linea;
             for (i = 0; i < 10; i++)
@@ -26,6 +28,23 @@ namespace numeracion1_100
             Console.WriteLine("La suma es: {0}", suma);
             promedio = suma / 10;
             Console.Write("La promedio es: {0}", promedio);
+            */
+            int i, msiete = 0, mensiete = 0, nota;
+            for(i=1; i <= 10; i++)
+            {
+                Console.Write("ingresa calificacion: ");
+                nota = int.Parse(Console.ReadLine());
+                if (nota >= 7)
+                {
+                    msiete = msiete + 1;
+                }
+                else
+                {
+                    mensiete = mensiete + 1;
+                }
+            }
+            Console.WriteLine("Cantidad de alumnos con promedio mayor o igual a 7: {0}",msiete);
+            Console.WriteLine("Cantidad de alumnos con promedio menor a 7: {0}", mensiete);
         }
     }
 }
