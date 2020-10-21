d79414ac3cab792ba9619b4e15c8b9132b8ccf23
AndresYzimar CanoRios <acano4@ucol.mx>
2020-10-21 09:15:22 -0500

    1

---
+++ numeracion1-100/numeracion1-100/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace numeracion1_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i <= 100; i++)
+            {
+                Console.Write(i);
+                Console.Write("-");
+            }
+        }
+    }
+}
