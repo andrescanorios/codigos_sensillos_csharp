da3cd5e1cda69b097f4fd0d3a7efa9d7b647c251
AndresYzimar CanoRios <acano4@ucol.mx>
2020-10-28 09:21:24 -0600

    h

---
+++ multiplos/multiplos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabla;
+            Console.WriteLine("Introduce el valor de la tabla: ");
+            tabla = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++) 
+            {
+                Console.WriteLine(tabla, " X ", i, " = ", tabla * i);
+            }
+        }
+    }
+}
